94d10395ca4414ffaaca8f25791b72b2941b988d
Younes202 <younessghyer642@gmail.com>
2023-09-03 23:10:00 +0100

    Set all necessery components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
   return (
-   
-    <p>HI THere</p>
+   <div>
+        <Sidebar />
+
+
+   </div>
+
   );
 }
 
