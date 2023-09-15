8f1d88a8abfa942388fb396b9dc2666b04a3c001
Younes202 <younessghyer642@gmail.com>
2023-09-15 18:50:54 +0100

    Create a navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import './App.css';
+import './styles/tailwind.css';
+import React from 'react';
+import Navbar from './components/Navbar';
 
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Skills from './components/Skills';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+
 function App() {
   return (
-   <div>
-        <Sidebar />
+    <div>
+          <Navbar />
 
-
-   </div>
-
+    <div className="flex-1 bg-gray-200 p-10">
+      {/* Main content */}
+    </div>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import your main App component or entry point here
+import './index.css'; // You may also have an index.css file for global styles
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
